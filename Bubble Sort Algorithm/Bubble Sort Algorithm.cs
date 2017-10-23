de4c5ddbf68c3504682b4e1bc93bb4b63fa61063
skipter@abv.bg <b0r1s-12345>
2017-10-23 08:43:17 +0300

    Some basic algorithms

---
+++ Bubble Sort Algorithm/Bubble Sort Algorithm.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp13
+{
+    using System;
+    using System.Linq;
+
+    public class TestingAlgoritums
+    {
+        public static void Main()
+        {
+            // Input array
+            int[] arr = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int temp = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int k = 0; k < arr.Length - 1; k++)
+                {
+                    if (arr[k] > arr[k + 1])        // With > / < reverse the sorting flow
+                    {
+                        temp = arr[k + 1];
+                        arr[k + 1] = arr[k];
+                        arr[k] = temp;
+                    }
+                }
+            }
+            //Print result array
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+        }
+    }
+}
---
+++ Fibonaci Algorithm/Fibonacci Algorithm.cs
@@ -0,0 +1,28 @@
+﻿namespace Fibonaci_Algorithm
+{
+    using System;
+    public class Program
+    {
+        public static void Main()
+        {
+
+            int num = int.Parse(Console.ReadLine());
+            for (int k = 0; k < num; k++)
+            {
+                Console.WriteLine(Fibonacci(k));
+            }
+        }
+        public static int Fibonacci(int num)            //Method
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < num; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+    }
+}
